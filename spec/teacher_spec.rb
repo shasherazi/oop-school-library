bd79c9a29545bd6da563806597a3ca5ec584cc5b
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-19 19:51:52 +0100

    Fix errors

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -5,13 +5,15 @@ describe Teacher do
   before(:each) do
     @teacher = Teacher.new(20, 'Michael', nil, 'Software Engineering')
   end
-
-  it 'should create a new teacher' do
-    teacher = Teacher.new(20, 'Michael', nil, 'Software Engineering')
-    expect(teacher.class.name).to eq('Teacher')
+  context 'when creating a new book' do
+    it 'should create a new student' do
+      teacher = Teacher.new(20, 'Michael', nil, 'Software Engineering')
+      expect(teacher.class.name).to eq('Teacher')
+    end
   end
-
-  it 'teacher can use services' do
-    expect(@teacher.can_use_services?).to eq(true)
+  context 'when I call the can_use_services? method' do
+    it 'Teacher should be able to use services' do
+      expect(@teacher.can_use_services?).to eq(true)
+    end
   end
 end
